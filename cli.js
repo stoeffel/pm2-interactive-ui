d5db7ee60d50ebe151e53ed432bd4fe011555d34
stoeffel <schtoeffel@gmail.com>
2015-04-16 12:57:40 +0200

    add --version

--- cli.js
+++ cli.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 var pm2I = require('./index.js');
-var argv = require('yargs').argv;
+var argv = require('yargs').version(function() {
+    return require('./package').version;
+  }).argv;
 
 pm2I(argv._[0]);
